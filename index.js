9751924753cddcd959f3b50c873acbf78a8af5be
JackLabukas <labukasgediminas@gmail.com>
2023-01-27 21:37:43 +0000

    added logic to index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
-
+const team = [];
 const questionsManager = [
   {
     type: "input",
@@ -28,13 +28,16 @@ const questionsManager = [
     message: "Your Manager's Email please?",
     name: "email",
   },
-];
+  {
+    type: "input",
+    message: "Your Manager's office number?",
+    name: "officeNumber",
+  },
 
-const questionsRole = [
   {
     type: "list",
     message: "Chose an option please",
-    choices: ["Engineer", "Intern", "Finish Building Team"],
+    choices: ["Engineer", "Intern", "FinishBuildingTeam"],
     name: "option",
   },
 ];
@@ -43,57 +46,81 @@ const questionsEngineer = [
   {
     type: "input",
     message: "Engineer's Name please?",
-    name: "name",
+    name: "nameEngineer",
   },
   {
     type: "input",
     message: "Your Engineer's ID please?",
-    name: "id",
+    name: "idEngineer",
   },
   {
     type: "input",
     message: "Your Engineer's Email please?",
-    name: "email",
+    name: "emailEngineer",
   },
   {
     type: "input",
     message: "Your GitHub Username",
-    name: "username",
+    name: "usernameGitHubEngineer",
   },
 ];
-
 const questionsIntern = [
   {
     type: "input",
     message: "Intern's Name please?",
-    name: "name",
+    name: "nameIntern",
   },
   {
     type: "input",
     message: "Your Intern's ID please?",
-    name: "id",
+    name: "idIntern",
   },
   {
     type: "input",
     message: "Your Intern's Email please?",
-    name: "email",
+    name: "emailIntern",
   },
   {
     type: "input",
     message: "Your Intern's School please?",
-    name: "school",
+    name: "schoolIntern",
   },
 ];
 
-function writeToFile(fileName, data) {
-  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
-}
+// function writeToFile(fileName, data) {
+//   return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+// }
 
 function init() {
   inquirer.prompt(questionsManager).then((response) => {
-    // writeToFile("test.txt", render({ ...response }));
-    // console.log(response);
+    // writeToFile("test.html", render(manager));
+    // render(manager);
+    const manag = new Manager(
+      response.name,
+      response.id,
+      response.email,
+      response.officeNumber
+    );
+    team.push(manag);
+    if (response.option === "Engineer") {
+      inquirer.prompt(questionsEngineer).then((response) => {
+        const engin = new Engineer(
+          response.nameEngineer,
+          response.idEngineer,
+          response.emailEngineer,
+          response.usernameGitHubEngineer
+        );
+        console.log(response);
+        team.push(engin);
+        console.log(team);
+      });
+    } else if (response.option === "Intern") {
+      inquirer.prompt(questionsEngineer).then((response) => {
+        const internUser = 
+      });
+    } else if (response.option === "FinishBuildingTeam") {
+    }
+    // console.log(render(manager));
   });
 }
-
 init();
