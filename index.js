e708a3f8e6639d4cb614897a06da8200b93a5104
JackLabukas <labukasgediminas@gmail.com>
2023-01-27 19:47:18 +0000

    added questions

--- index.js
+++ index.js
@@ -10,6 +10,90 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const questionsManager = [
+  {
+    type: "input",
+    message: "Your Manager's Name please?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "Your Manager's ID please?",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "Your Manager's Email please?",
+    name: "email",
+  },
+];
+
+const questionsRole = [
+  {
+    type: "list",
+    message: "Chose an option please",
+    choices: ["Engineer", "Intern", "Finish Building Team"],
+    name: "option",
+  },
+];
+
+const questionsEngineer = [
+  {
+    type: "input",
+    message: "Engineer's Name please?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "Your Engineer's ID please?",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "Your Engineer's Email please?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "Your GitHub Username",
+    name: "username",
+  },
+];
+
+const questionsIntern = [
+  {
+    type: "input",
+    message: "Intern's Name please?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "Your Intern's ID please?",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "Your Intern's Email please?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "Your Intern's School please?",
+    name: "school",
+  },
+];
+
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+function init() {
+  inquirer.prompt(questionsManager).then((response) => {
+    // writeToFile("test.txt", render({ ...response }));
+    // console.log(response);
+  });
+}
+
+init();
